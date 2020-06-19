bb5ef690fe5260a382ab4c44339612324714eb28
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-19 18:30:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,25 +18,17 @@ def retrieval
 end
 
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
-
-
-
 def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+  shipping
+end
+
   # add 2 muskets to the shipping_manifest hash below
-
-
 
   # add 4 gun powder to the shipping_manifest hash below
 
-
-
   # return the shipping_manifest hash below
-
-end
