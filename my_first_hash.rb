48d37a2420da1f495c440374ae80ddced6fbd77e
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-19 18:21:42 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,5 +1,4 @@
-def my_hash(your_hash)
-  my_hash = {"Name" => "Michael", "age" => "28"}
+def my_hash(your_hash = {"Name" => "Michael", "age" => "28"})
 end
 
 # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
